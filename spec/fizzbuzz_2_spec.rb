c8018321d911b6ded5e6c36bf96acb605ecd7f3e
Jamie LeMercier <jamielemercier@gmail.com>
2017-10-18 16:29:08 +0100

    Adds test for multiples of 5

--- spec/fizzbuzz_2_spec.rb
+++ spec/fizzbuzz_2_spec.rb
@@ -4,4 +4,10 @@ describe 'fizzbuzz' do
   it 'returns fizz when 3' do
     expect(3.fizzbuzz).to eq 'fizz'
   end
+  it 'returns fizz when 6' do
+    expect(6.fizzbuzz).to eq 'fizz'
+  end
+  it 'returns fizz when 5' do
+    expect(5.fizzbuzz).to eq 'buzz'
+  end
 end
